6dfc0fdc4f5d3908de88e811ba264092cc3e3f96
sanqi <sanqi1021@163.com>
2021-08-05 16:47:50 +0800

    third commit

--- src/main/java/san/qi/git/GitTest.java
+++ src/main/java/san/qi/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello GitHub!");
+        System.out.println("Hello sanqi!");
 
     }
 }
