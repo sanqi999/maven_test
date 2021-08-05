f575d675dca21a0f78f086e3ee36b41be71f9f0b
sanqi <sanqi1021@163.com>
2021-08-05 16:42:35 +0800

    second commit

--- src/main/java/san/qi/git/GitTest.java
+++ src/main/java/san/qi/git/GitTest.java
@@ -7,5 +7,7 @@ package san.qi.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello GitHub!");
+
     }
 }
