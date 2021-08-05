3c08727d187a20c451c4eaf4082bddff32af855f
sanqi <sanqi1021@163.com>
2021-08-05 17:24:40 +0800

    push commit

--- src/main/java/san/qi/git/GitTest.java
+++ src/main/java/san/qi/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("Hello tao!");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
 
     }
 }
