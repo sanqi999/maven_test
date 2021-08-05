c073a6029a913e0eba7a4a253e71fa614f84bf38
sanqi <sanqi1021@163.com>
2021-08-05 16:56:05 +0800

    hot-fix commit1

--- src/main/java/san/qi/git/GitTest.java
+++ src/main/java/san/qi/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("Hello GitHub!");
         System.out.println("Hello sanqi!");
+        System.out.println("Hello tao!");
 
     }
 }
