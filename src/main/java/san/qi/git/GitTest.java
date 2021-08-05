d3daad37640e3b6ceb9510efa4f5efa7b2dc7446
sanqi <sanqi1021@163.com>
2021-08-05 16:39:44 +0800

    first commit

---
+++ src/main/java/san/qi/git/GitTest.java
@@ -0,0 +1,11 @@
+package san.qi.git;
+
+/**
+ * @author sanqi
+ * @create 2021-08-05 16:35
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
